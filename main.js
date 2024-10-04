36024694f5e1c6ee1a724b9fab120b8e3cd74715
Vitor Costa <133382691+vitorkkkk@users.noreply.github.com>
2024-10-04 11:39:32 -0300

    main.js

--- main.js
+++ main.js
@@ -1,10 +1,42 @@
-var titulo = document.querySelector("h1");
-titulo.textContent = "Vitor Nutrição";
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "joao Nutrição";
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector (".info-peso");
+for(var i = 0; i < 5; i ++) {
+  console.log(pacientes[i]);
+  
+  var tdPeso = pacientes[i].querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector (".info-altura");
+var tdAltura = pacientes[i].querySelector(".info-altura");
 var altura = tdAltura.textContent;
+ 
+ var tdImc = pacientes[i].querySelector(".info-imc");
+
+ 
+console.log(peso);
+console.log(altura);
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+
+if(peso < 0 || peso > 600) {
+   tdImc.textContent = "Peso inválido!";
+   pesoEhValido = false;
+   pacientes[i].classList.add("dados-invalidos");
+   }
+ 
+if(altura < 0 || altura > 2.80) {
+  tdImc.textContent = "altura inválida!"
+  alturaEhValida = false;
+  pacientes[i].classList.add("dados-invalidos");
+}
+
+if (pesoEhValido && alturaEhValida) {
+ var imc = peso / (altura * altura);
+  tdImc.textContent = imc.toFixed(2);
+}
+
+}
